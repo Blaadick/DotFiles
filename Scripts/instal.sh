9d399b6245c3580619cc743a38ffc617251d53ca
Blaadick <lablaadick@gmail.com>
2024-09-16 22:11:31 +0300

    Completing the install script

--- Scripts/instal.sh
+++ Scripts/instal.sh
@@ -1,2 +1,13 @@
 ./update.sh
-sudo pacman -S hyprpaper grim slurp btop code nautilus firefox torbrowser-launcher discord steam
+sudo pacman -S hyprpaper wl-clipboard brightnessctl git grim slurp nwg-look blueman code nautilus firefox torbrowser-launcher discord steam prismlauncher btop ttf-jetbrains-mono
+sudo pacman -Rns dolphin vim htop
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+
+yay -S eww jetbrains-toolbox youtube-music-bin
+
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install bobthefish
