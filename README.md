15744eb3bd91ed2578af70e690fe6f3d70e87089
Blaadick <lablaadick@gmail.com>
2024-09-24 23:05:21 +0300

    Simple update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # DotFiles
+Created for my personal use, but you can use this piece of crap any way you want if you're happy with it
+
+# Install
+Works only with __Arch Linux__ and __Hyprland__
+
+```shell
+git clone https://github.com/Blaadick/DotFiles ~/DotFiles
+~/DotFiles/Scripts/install.sh
+```
--- Scripts/instal.sh
+++ Scripts/instal.sh
@@ -1,13 +1,27 @@
-./update.sh
-sudo pacman -S hyprpaper wl-clipboard brightnessctl git grim slurp nwg-look blueman code nautilus firefox torbrowser-launcher discord steam prismlauncher btop ttf-jetbrains-mono
+sudo pacman -Syu
+sudo pacman -S hyprpaper wl-clipboard brightnessctl git grim slurp flatpak nwg-look blueman code nautilus firefox torbrowser-launcher discord steam prismlauncher btop ttf-jetbrains-mono
 sudo pacman -Rns dolphin vim htop
 
-git clone https://aur.archlinux.org/yay.git
+flatpak install flathub net.blockbench.Blockbench
+
+cd /var/lib
+sudo git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd
 
-yay -S eww jetbrains-toolbox youtube-music-bin
+yay -S eww jetbrains-toolbox youtube-music-bin hyprpicker
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install bobthefish
+
+cd ~/DotFiles
+cp -a Configs/* ~/.config
+cp -a Pictures/* ~/Pictures
+cd
+
+cd ~/.local/share
+git clone https://github.com/vinceliuice/Graphite-gtk-theme
+cd Graphite-gtk-theme
+./install.sh -d ~/.themes -c dark --tweaks black rimless normal -l
+cd
