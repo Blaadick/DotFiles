5bba84651ed1b5212a3a66ade72b019751da4bfc
Blaadick <lablaadick@gmail.com>
2025-01-06 00:12:06 +0300

    Update wallpapers to 4k

---
+++ Scripts/ComfyUI/launch.sh
@@ -0,0 +1,2 @@
+source venv/bin/activate
+./venv/bin/python main.py --normalvram --disable-smart-memory --force-fp16 --auto-launch
---
+++ Scripts/install.sh
@@ -0,0 +1,30 @@
+sudo pacman -Syu
+sudo pacman -S $(cat aur_official_packages.lst | grep -v "#")
+sudo pacman -Rns dolphin vim htop
+
+flatpak install flathub net.blockbench.Blockbench
+
+git clone https://aur.archlinux.org/yay.git ~/yay
+cd ~/yay
+makepkg -si
+cd
+
+yay -S $(cat aur_unofficial_packages.lst | grep -v "#")
+
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf install bobthefish
+
+cd ~/DotFiles
+cp -a Configs/* ~/.config
+mkdir ~/.icons
+cp -a Icons/* ~/.icons
+cp -a Pictures/* ~/Pictures
+mkdir ~/Scripts
+cp -a Scripts/* ~/Scripts
+cd
+
+cd ~/.local/share
+git clone https://github.com/vinceliuice/Graphite-gtk-theme
+cd Graphite-gtk-theme
+./install.sh -d ~/.themes -c dark --tweaks black rimless normal -l
+cd
