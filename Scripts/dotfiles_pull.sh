354036bd57c433eb8e01b806c4c6b4a077712df6
Blaadick <lablaadick@gmail.com>
2025-02-26 18:08:55 +0300

    Return fontconfig

--- Scripts/dotfiles_pull.sh
+++
@@ -1,8 +0,0 @@
-cd ~/DotFiles
-
-git pull
-
-cp -a Configs/* ~/.config
-cp -a Icons/* ~/.icons
-cp -a Pictures/* ~/Pictures
-cp -a Scripts/* ~/Scripts
--- dotfiles_push.sh
+++ dotfiles_push.sh
@@ -12,6 +12,8 @@ cp -a ~/.config/kitty/ ~/DotFiles/Configs/
 cp -a ~/.config/nvim/ ~/DotFiles/Configs/
 cp -a ~/.config/vesktop/settings ~/DotFiles/Configs/vesktop/settings/
 cp -a ~/.config/fish/config.fish ~/DotFiles/Configs/fish/
+cp -a ~/.config/fontconfig/ ~/DotFiles/Configs/
+
 cp -a ~/.config/hyprswitch.css ~/DotFiles/Configs/
 
 cp -a ~/.icons/* ~/DotFiles/Icons/
